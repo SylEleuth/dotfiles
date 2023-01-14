384d584bb35e87ee76b71dbcb5e46a4a24ffcb31
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-01-14 20:45:45 +0000

    changed width

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -34,7 +34,7 @@ nvim_tree.setup {
   view = {
     adaptive_size = false,
     centralize_selection = false,
-    width = 30,
+    width = 50,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
