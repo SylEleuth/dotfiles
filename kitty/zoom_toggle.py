4c3a6bfe01114fab7e7d50cdbac7fe8e666124ec
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-01-27 11:26:01 +0000

    small changes

--- kitty/zoom_toggle.py
+++ kitty/zoom_toggle.py
@@ -1,12 +1,20 @@
-def main(args):
+from typing import List
+
+from kitty.boss import Boss
+
+
+def main(args: List[str]):
     pass
 
+
 from kittens.tui.handler import result_handler
+
+
 @result_handler(no_ui=True)
-def handle_result(args, answer, target_window_id, boss):
+def handle_result(args: List[str], answer: str, target_window_id: int, boss: Boss):
     tab = boss.active_tab
     if tab is not None:
-        if tab.current_layout.name == 'stack':
+        if tab.current_layout.name == "stack":
             tab.last_used_layout()
         else:
-            tab.goto_layout('stack')
+            tab.goto_layout("stack")
