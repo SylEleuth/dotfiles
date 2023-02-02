d5726a916ca9b70637664440f4373cb159e98660
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-02-02 12:06:22 +0000

    added boole config

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -7,6 +7,7 @@
 local status_ok, comment = pcall(require, "Comment")
 local status_ok, hlslens = pcall(require, "hlslens")
 local status_ok, no_neck_pain = pcall(require, "no-neck-pain")
+local status_ok, boole = pcall(require, "boole")
 if not status_ok then
   return
 end
@@ -33,5 +34,12 @@ no_neck_pain.setup({
     NvimTree = {
       position = "left",
     },
+  },
+})
+
+boole.setup({
+  mappings = {
+    increment = '<C-a>',
+    decrement = '<C-x>'
   },
 })
