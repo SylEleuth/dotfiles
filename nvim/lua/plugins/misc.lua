06c401653ecf6a34cec5084587ce256f0eb63026
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-02-13 14:30:34 +0000

    added tint and reticle config, commented colorful-winsep

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -8,6 +8,8 @@ local status_ok, comment = pcall(require, "Comment")
 local status_ok, hlslens = pcall(require, "hlslens")
 local status_ok, no_neck_pain = pcall(require, "no-neck-pain")
 local status_ok, boole = pcall(require, "boole")
+local status_ok, tint = pcall(require, "tint")
+local status_ok, reticle = pcall(require, "reticle")
 if not status_ok then
   return
 end
@@ -15,13 +17,13 @@ end
 comment.setup()
 hlslens.setup()
 
-require("colorful-winsep").setup({
-  highlight = {
-    bg = "#282828",
-    fg = "#504945",
-  },
-  no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree" },
-})
+-- require("colorful-winsep").setup({
+--   highlight = {
+--     bg = "#282828",
+--     fg = "#504945",
+--   },
+--   no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree" },
+-- })
 
 
 no_neck_pain.setup({
@@ -43,3 +45,21 @@ boole.setup({
     decrement = '<C-x>'
   },
 })
+
+tint.setup({
+  tint = -5,  -- Darken colors, use a positive value to brighten
+  saturation = 0.6,  -- Saturation to preserve
+  transforms = require("tint").transforms.SATURATE_TINT,  -- Showing default behavior, but value here can be predefined set of transforms
+  tint_background_colors = true,  -- Tint background portions of highlight groups
+  highlight_ignore_patterns = { "WinSeparator", "Status.*" },  -- Highlight group patterns to ignore, see `string.find`
+  window_ignore_function = function(winid)
+    local bufid = vim.api.nvim_win_get_buf(winid)
+    local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+    local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+
+    -- Do not tint `terminal` or floating windows, tint everything else
+    return buftype == "terminal" or floating
+  end
+})
+
+reticle.setup{}
