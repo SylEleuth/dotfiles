73023811fdb1cd8e60b1019120c17176edbb50e1
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-02-13 14:29:01 +0000

    moved back command search from middle screen popup to corner over command line

--- nvim/lua/plugins/wilder.lua
+++ nvim/lua/plugins/wilder.lua
@@ -30,46 +30,77 @@ local highlighters = {
   wilder.basic_highlighter(),
 }
 
+-- ** Commands above statusline and wildmenu in statusline
 wilder.set_option('renderer',  wilder.renderer_mux({
-  [':'] = wilder.popupmenu_renderer(
-    wilder.popupmenu_palette_theme({
-      -- 'single', 'double', 'rounded' or 'solid'
-      -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
-      border = 'rounded',
-      min_height = 0,
-      max_height = '50%',
-      min_width = '20%',
-      max_width = '40%',
-      prompt_position = 'bottom',
-      reverse = 0,
+  [':'] = wilder.popupmenu_renderer({
     highlighter = wilder.basic_highlighter(),
-    highlights = {
-      accent = wilder.make_hl('WilderAccent', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#83a598'}}),
-      selected = wilder.make_hl('MyHighlights1', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#fe8019', background = '#282828'}}),
-      -- default = wilder.make_hl('MyHighlights', 'Pmenu', {{a = 1}, {a = 1}, {background = '282828'}}),
+    left = {
+      ' ',
+      wilder.popupmenu_devicons(),
     },
-      left = {
-        ' ',
-        wilder.popupmenu_devicons(),
-        wilder.popupmenu_buffer_flags({
-          flags = ' a + ',
-          icons = {['+'] = '', a = '', h = ''},
-        }),
-      },
-      right = {' ', wilder.popupmenu_scrollbar()},
-    })
-  ),
+    right = {' ', wilder.popupmenu_scrollbar()},
+    highlights = {
+      default = wilder.make_hl('MyHighlights1', 'Pmenu', {{a = 1}, {a = 1}, {background = '#1d2021'}}),
+      selected = wilder.make_hl('MyHighlights2', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#fe8019', background = '#282828'}}),
+      accent = wilder.make_hl('WilderAccent1', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#83a598', background = "#1d2021"}}),
+    },
+  }),
   ['/'] = wilder.wildmenu_renderer({
     highlighter = wilder.pcre2_highlighter(),
     highlights = {
-      default = wilder.make_hl('MyHighlights', 'Pmenu', {{a = 1}, {a = 1}, {background = '#282828'}}),
-      accent = wilder.make_hl('WilderAccent1', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#83a598', background = '#282828'}}),
-      selected = wilder.make_hl('MyHighlights2', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#fe8019', background = '#282828'}}),
+      default = wilder.make_hl('MyHighlights1', 'Pmenu', {{a = 1}, {a = 1}, {background = '#1d2021'}}),
+      selected = wilder.make_hl('MyHighlights3', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#fe8019', background = '#1d2021'}}),
+      accent = wilder.make_hl('WilderAccent2', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#83a598', background = '#1d2021'}}),
     },
     separator = ' · ',
     left = {'SEARCH', wilder.wildmenu_spinner(), ' '},
     right = {' ', wilder.wildmenu_index()},
   })
 }))
+
+
+
+-- ** Commands in the middle of the window and wildmenu in statusline
+-- wilder.set_option('renderer',  wilder.renderer_mux({
+--   [':'] = wilder.popupmenu_renderer(
+--     wilder.popupmenu_palette_theme({
+--       -- 'single', 'double', 'rounded' or 'solid'
+--       -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+--       border = 'rounded',
+--       min_height = 0,
+--       max_height = '50%',
+--       min_width = '20%',
+--       max_width = '40%',
+--       prompt_position = 'bottom',
+--       reverse = 0,
+--     highlighter = wilder.basic_highlighter(),
+--     highlights = {
+--       accent = wilder.make_hl('WilderAccent', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#83a598'}}),
+--       selected = wilder.make_hl('MyHighlights1', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#fe8019', background = '#282828'}}),
+--       -- default = wilder.make_hl('MyHighlights', 'Pmenu', {{a = 1}, {a = 1}, {background = '282828'}}),
+--     },
+--       left = {
+--         ' ',
+--         wilder.popupmenu_devicons(),
+--         wilder.popupmenu_buffer_flags({
+--           flags = ' a + ',
+--           icons = {['+'] = '', a = '', h = ''},
+--         }),
+--       },
+--       right = {' ', wilder.popupmenu_scrollbar()},
+--     })
+--   ),
+--   ['/'] = wilder.wildmenu_renderer({
+--     highlighter = wilder.pcre2_highlighter(),
+--     highlights = {
+--       default = wilder.make_hl('MyHighlights', 'Pmenu', {{a = 1}, {a = 1}, {background = '#282828'}}),
+--       accent = wilder.make_hl('WilderAccent1', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#83a598', background = '#282828'}}),
+--       selected = wilder.make_hl('MyHighlights2', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#fe8019', background = '#282828'}}),
+--     },
+--     separator = ' · ',
+--     left = {'SEARCH', wilder.wildmenu_spinner(), ' '},
+--     right = {' ', wilder.wildmenu_index()},
+--   })
+-- }))
 
 vim.keymap.set('n', '<leader>wd', ':call wilder#toggle()<cr>')
