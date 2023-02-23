3adfb228e6cab43b862737df2c9e5bb93e1ed216
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-02-23 14:07:07 +0000

    formatting

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -48,11 +48,11 @@ boole.setup({
 })
 
 tint.setup({
-  tint = -5,  -- Darken colors, use a positive value to brighten
-  saturation = 0.6,  -- Saturation to preserve
-  transforms = require("tint").transforms.SATURATE_TINT,  -- Showing default behavior, but value here can be predefined set of transforms
-  tint_background_colors = true,  -- Tint background portions of highlight groups
-  highlight_ignore_patterns = { "WinSeparator", "Status.*" },  -- Highlight group patterns to ignore, see `string.find`
+  tint = -5, -- Darken colors, use a positive value to brighten
+  saturation = 0.6, -- Saturation to preserve
+  transforms = require("tint").transforms.SATURATE_TINT, -- Showing default behavior, but value here can be predefined set of transforms
+  tint_background_colors = true, -- Tint background portions of highlight groups
+  highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
   window_ignore_function = function(winid)
     local bufid = vim.api.nvim_win_get_buf(winid)
     local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
@@ -63,4 +63,4 @@ tint.setup({
   end
 })
 
-reticle.setup{}
+reticle.setup {}
