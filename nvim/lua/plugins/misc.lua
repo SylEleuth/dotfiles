b8b8ff63adc47a769bb100d7f8de1373370c8796
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-03-11 20:44:53 +0000

    fixed no-neck-pain settings, commented tint

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,14 +1,13 @@
 -----------------------------------------------------------
 -- Configuration file for miscelanous plugins
 -----------------------------------------------------------
-
 -- https://github.com/shortcuts/no-neck-pain.nvim
 
 local status_ok, comment = pcall(require, "Comment")
 local status_ok, hlslens = pcall(require, "hlslens")
 local status_ok, no_neck_pain = pcall(require, "no-neck-pain")
 local status_ok, boole = pcall(require, "boole")
-local status_ok, tint = pcall(require, "tint")
+-- local status_ok, tint = pcall(require, "tint")
 local status_ok, reticle = pcall(require, "reticle")
 local status_ok, leap = pcall(require, "leap")
 if not status_ok then
@@ -31,11 +30,16 @@ hlslens.setup()
 no_neck_pain.setup({
   width = 100,
   mappings = {
-    toggle = "<F5>"
+    enabled = true,
+    toggle = "<Leader>np",
+    widthUp = "<Leader>n=",
+    widthDown = "<Leader>n-",
+    scratchPad = "<Leader>ns",
   },
   integrations = {
     NvimTree = {
       position = "left",
+      reopen = true,
     },
   },
 })
@@ -47,21 +51,21 @@ boole.setup({
   },
 })
 
-tint.setup({
-  tint = -5, -- Darken colors, use a positive value to brighten
-  saturation = 0.6, -- Saturation to preserve
-  transforms = require("tint").transforms.SATURATE_TINT, -- Showing default behavior, but value here can be predefined set of transforms
-  tint_background_colors = true, -- Tint background portions of highlight groups
-  highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
-  window_ignore_function = function(winid)
-    local bufid = vim.api.nvim_win_get_buf(winid)
-    local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
-    local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+-- tint.setup({
+--   tint = -5, -- Darken colors, use a positive value to brighten
+--   saturation = 0.6, -- Saturation to preserve
+--   transforms = require("tint").transforms.SATURATE_TINT, -- Showing default behavior, but value here can be predefined set of transforms
+--   tint_background_colors = true, -- Tint background portions of highlight groups
+--   highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
+--   window_ignore_function = function(winid)
+--     local bufid = vim.api.nvim_win_get_buf(winid)
+--     local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+--     local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
 
-    -- Do not tint `terminal` or floating windows, tint everything else
-    return buftype == "terminal" or floating
-  end
-})
+--     -- Do not tint `terminal` or floating windows, tint everything else
+--     return buftype == "terminal" or floating
+--   end
+-- })
 
 reticle.setup {}
 
