b34bb3554549862e4ebee46ac41cad308d8e2dd3
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2023-07-03 21:27:59 +0100

    removed variable fromm filter

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -103,17 +103,16 @@ aerial.setup ({
   -- A list of all symbols to display. Set to false to display all symbols.
   -- This can be a filetype map (see :help aerial-filetype-map)
   -- To see all available values, see :help SymbolKind
-  -- filter_kind = {
-  --   "Class",
-  --   "Constructor",
-  --   "Enum",
-  --   "Function",
-  --   "Interface",
-  --   "Module",
-  --   "Method",
-  --   "Struct",
-  --   "Variable",
-  -- },
+  filter_kind = {
+    "Class",
+    "Constructor",
+    "Enum",
+    "Function",
+    "Interface",
+    "Module",
+    "Method",
+    "Struct",
+  },
   filter_kind = false,
 
   -- Determines line highlighting mode when multiple splits are visible.
