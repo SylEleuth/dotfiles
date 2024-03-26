9d8c1a2610b8ff9374f0ba250947cb1bb9e6693e
SylEleuth <33354262+SylEleuth@users.noreply.github.com>
2024-03-26 11:07:51 +0000

    added to repository

---
+++ nvim/lua/packer_init_test.lua
@@ -0,0 +1,64 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
+-- Automatically install packer
+
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+vim.opt.laststatus = 3
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+end
+
+-- Autocommand that reloads neovim whenever you save the packer_init.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
+-- Packer in flaoting window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+  git = {
+    clone_timeout = 300, -- Timeout, in seconds, for git clones
+  },
+}
+
+-- Install plugins
+return packer.startup(function(use)
+
+use {
+    "nvim-zh/colorful-winsep.nvim",
+    config = function ()
+        require('colorful-winsep').setup()
+    end
+}
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
